11c7213e1766d3120204549517f04f33f7004b1c
belemaaa <graceitamunoala@gmail.com>
2024-03-14 09:22:42 +0100

    set up image service using CloudinaryDotNet

---
+++ Helpers/CloudinarySettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace _netstore.Helpers
+{
+	public class CloudinarySettings
+	{
+		public string? CloudName { get; set; }
+
+		public string? ApiKey { get; set; }
+
+		public string? ApiSecret { get; set; }
+	}
+}
+
